e1e22a2e060de44aa666b2bc262298314b206e9e
CHOSSEANG <jng00153@gmail.com>
2025-09-29 20:56:52 +0900

    초기 프로젝트 생성

---
+++ src/main/java/icd3/commerce/beteacher/BeteacherApplication.java
@@ -0,0 +1,13 @@
+package icd3.commerce.beteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeteacherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icd3/commerce/beteacher/BeteacherApplicationTests.java
@@ -0,0 +1,13 @@
+package icd3.commerce.beteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeteacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
