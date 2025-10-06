c3752d710faeaf960b8dfba2bec241f826ff105a
CHOSSEANG <jng00153@gmail.com>
2025-10-06 20:34:41 +0900

    items에 lectures와의 연관관계 추가

--- src/main/java/icd3/commerce/beteacher/sbb/items.java
+++ src/main/java/icd3/commerce/beteacher/sbb/items.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +18,9 @@ public class items {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
+	@ManyToOne
+	Lectures lectures;
 
 	private String keyword;
 
