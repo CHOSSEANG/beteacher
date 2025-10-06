061164b5afc97db4085d4453b49ea6503381d637
CHOSSEANG <jng00153@gmail.com>
2025-10-06 20:34:27 +0900

    lectures 엔티티에 column 추가

--- src/main/java/icd3/commerce/beteacher/sbb/Lectures.java
+++ src/main/java/icd3/commerce/beteacher/sbb/Lectures.java
@@ -2,6 +2,7 @@ package icd3.commerce.beteacher.sbb;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,9 +20,12 @@ public class Lectures {
 
 	private String name;
 
+	@Column(name = "created_at")
 	private LocalDateTime createdAt;
 
+	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 
+	@Column(name = "deleted_at")
 	private LocalDateTime deletedAt;
 }
