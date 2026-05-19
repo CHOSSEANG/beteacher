e8d22fc18c7670cd0f0a35af1052b66fdd1ecfc7
CHOSSEANG <jng00153@gmail.com>
2026-05-19 17:39:02 +0900

    feat : keywordservice CRUD 작성

---
+++ src/main/java/icd3/commerce/beteacher/service/KeywordService.java
@@ -0,0 +1,36 @@
+package icd3.commerce.beteacher.service;
+
+import org.springframework.stereotype.Service;
+
+import icd3.commerce.beteacher.entity.Keyword;
+import icd3.commerce.beteacher.repository.KeywordRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class KeywordService {
+
+	private final KeywordRepository keywordRepository;
+
+	public Keyword save(Keyword keyword) {
+		return keywordRepository.save(keyword);
+	}
+
+	public Keyword update(Keyword keyword) {
+		keywordRepository.findById(keyword.getId())
+			.orElseThrow(() -> new IllegalArgumentException("Keyword not found"));
+		return keywordRepository.save(keyword);
+	}
+
+	public Keyword read(long keywordId){
+		return keywordRepository.findById(keywordId).get();
+	}
+
+	public String delete(long keywordId){
+		keywordRepository.findById(keywordId)
+			.orElseThrow(() -> new IllegalArgumentException("Keyword not found"));
+		keywordRepository.deleteById(keywordId);
+
+		return "Keyword deleted successfully";
+	}
+}
