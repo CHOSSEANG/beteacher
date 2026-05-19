5bcf9839d8adbe44fa83ef029fdbb479c59f4bd2
CHOSSEANG <jng00153@gmail.com>
2026-05-19 18:01:59 +0900

    feat : id 타입 변경

--- src/main/java/icd3/commerce/beteacher/entity/Keyword.java
+++ src/main/java/icd3/commerce/beteacher/entity/Keyword.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 public class Keyword {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 
 	@ManyToOne
 	Category categories;
