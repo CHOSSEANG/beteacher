9dc8a0e4675cae25ad05ee4ea7c344da742e9ce5
CHOSSEANG <jng00153@gmail.com>
2026-05-20 10:29:18 +0900

    feat : repository 어노테이션 추가

--- src/main/java/icd3/commerce/beteacher/repository/KeywordRepository.java
+++ src/main/java/icd3/commerce/beteacher/repository/KeywordRepository.java
@@ -1,9 +1,11 @@
 package icd3.commerce.beteacher.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import icd3.commerce.beteacher.entity.Keyword;
 
+@Repository
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
 }
