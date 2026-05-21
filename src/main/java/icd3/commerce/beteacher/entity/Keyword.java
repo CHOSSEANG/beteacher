dc8f5af9291250484af3128912efad9bcd79495e
CHOSSEANG <jng00153@gmail.com>
2026-05-21 14:39:32 +0900

    feat : 키워드 entity Builder 어노테이션 추가

--- src/main/java/icd3/commerce/beteacher/entity/Keyword.java
+++ src/main/java/icd3/commerce/beteacher/entity/Keyword.java
@@ -8,12 +8,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Builder                    // 추가
+@NoArgsConstructor          // 추가 (JPA 필수)
+@AllArgsConstructor         // 추가 (Builder 사용 시 필요)
 public class Keyword {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
