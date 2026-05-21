5e198574172942c09838e9473a6b9cefc5ca23b6
CHOSSEANG <jng00153@gmail.com>
2026-05-21 16:14:38 +0900

    Test : 키워드 조회 테스트코드 작성

--- src/test/java/icd3/commerce/beteacher/service/KeywordServiceTest.java
+++ src/test/java/icd3/commerce/beteacher/service/KeywordServiceTest.java
@@ -3,6 +3,8 @@ package icd3.commerce.beteacher.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -42,10 +44,22 @@ class KeywordServiceTest {
 	}
 
 	@Test
-	void read() {
+	void 키워드_조회_성공() {
+		Keyword keyword = Keyword.builder().id(1L).keyword("keyword1").content("content1").build();
+
+		given(keywordRepository.findById(1L)).willReturn(java.util.Optional.of(keyword));
+
+		assertThat(keywordService.read(1L)).isEqualTo(keyword);
 	}
 
 	@Test
-	void delete() {
+	void 키워드_조회_실패_존재하지않는_ID() {
+		// given
+		given(keywordRepository.findById(999L)).willReturn(Optional.empty());
+
+		// when & then
+		assertThatThrownBy(() -> keywordService.read(999L))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("Keyword not found");
 	}
 }
