cc8735bb682a683ff197ad75614a219b2d0c9813
CHOSSEANG <jng00153@gmail.com>
2026-06-26 22:14:15 +0900

    feat : User 기능 구현

---
+++ src/main/java/icd3/commerce/beteacher/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package icd3.commerce.beteacher.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import icd3.commerce.beteacher.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByName(String name);
+}
