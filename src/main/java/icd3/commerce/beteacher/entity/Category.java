8eb04d50314118a69e62ddc39d734de30e4a052b
CHOSSEANG <jng00153@gmail.com>
2026-06-28 23:36:56 +0900

    feat : DB 테이블명 변경

--- src/main/java/icd3/commerce/beteacher/entity/Category.java
+++ src/main/java/icd3/commerce/beteacher/entity/Category.java
@@ -10,12 +10,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "categories")
 public class Category {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
--- src/main/java/icd3/commerce/beteacher/entity/Note.java
+++ src/main/java/icd3/commerce/beteacher/entity/Note.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name ="notes")
 public class Note {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
