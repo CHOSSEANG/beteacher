fea0cf2c65b5dd6b9c6454f1861a752d94d5b848
CHOSSEANG <jng00153@gmail.com>
2026-06-28 22:46:50 +0900

    feat : entity에 User 항목 추가

--- src/main/java/icd3/commerce/beteacher/entity/Category.java
+++ src/main/java/icd3/commerce/beteacher/entity/Category.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +22,10 @@ public class Category {
 	private long id;
 
 	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
--- src/main/java/icd3/commerce/beteacher/entity/Keyword.java
+++ src/main/java/icd3/commerce/beteacher/entity/Keyword.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +29,10 @@ public class Keyword {
 
 	@ManyToOne
 	Category categories;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
