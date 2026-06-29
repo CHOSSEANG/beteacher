94e696168b77c5720c49ffaba23d6ab114372e0a
CHOSSEANG <jng00153@gmail.com>
2026-06-29 16:08:50 +0900

    feat : spring security 설정에 h2 사용을 위한 xframe 허용 옵션 추가

--- src/main/java/icd3/commerce/beteacher/auth/SecurityConfig.java
+++ src/main/java/icd3/commerce/beteacher/auth/SecurityConfig.java
@@ -17,11 +17,13 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(auth -> auth
-					.anyRequest().permitAll());
+		http.csrf(AbstractHttpConfigurer::disable);
 
+		http.authorizeHttpRequests(auth -> auth
+			.anyRequest().permitAll());
 
+		http.headers(headers -> headers
+			.frameOptions(frame -> frame.sameOrigin()));
 
 		http.formLogin(Customizer.withDefaults());
 
