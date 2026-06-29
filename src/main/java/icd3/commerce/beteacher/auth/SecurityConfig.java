7ce7073b603c8630dff19e0c61f28f9b985bb797
CHOSSEANG <jng00153@gmail.com>
2026-06-29 15:38:05 +0900

    feat : spring security 기본 로그인 페이지 사용하게 설정 변경

--- src/main/java/icd3/commerce/beteacher/auth/SecurityConfig.java
+++ src/main/java/icd3/commerce/beteacher/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package icd3.commerce.beteacher.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +23,7 @@ public class SecurityConfig {
 
 
 
-		http.formLogin(form -> form.loginPage("/login"));
+		http.formLogin(Customizer.withDefaults());
 
 		return http.build();
 	}
