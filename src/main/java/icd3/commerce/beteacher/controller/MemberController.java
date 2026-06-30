35bcb892cf6f60616b034aaf01bd7c540115d1c4
CHOSSEANG <jng00153@gmail.com>
2026-07-01 03:07:40 +0900

    feat : 회원가입 api 구현

---
+++ src/main/java/icd3/commerce/beteacher/controller/MemberController.java
@@ -0,0 +1,25 @@
+package icd3.commerce.beteacher.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import icd3.commerce.beteacher.entity.Member;
+import icd3.commerce.beteacher.repository.MemberRepository;
+import icd3.commerce.beteacher.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PostMapping
+	public Member signUp(@RequestBody Member member) {
+
+		return memberService.save(member);
+	}
+}
